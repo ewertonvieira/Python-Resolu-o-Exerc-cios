889103ab69cd5855b2d259b989199fac94d96625
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-12 20:19:15 -0300

    04

--- Questao_04.py
+++ Questao_04.py
@@ -1,11 +1,10 @@
 country_a = 80000 # 3% crescimento ao ano
 country_b = 200000 # 1.5% crescimento ao ano
 count = 0
-aux = 0
 # Numero de anos que A leva para superar B:
 while country_a < country_b:
-    country_a *= (3/100)
-    country_b *= (1.5/100)
+    country_a += country_a * 0.03
+    country_b += country_b * 0.015
     count += 1
     
 print(count)
