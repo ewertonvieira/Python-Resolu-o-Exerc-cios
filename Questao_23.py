bc5b39f2481dafad0deecdc0f3cfae4d0752d525
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-17 00:32:41 -0300

    23

--- Questao_23.py
+++ Questao_23.py
@@ -1,12 +1,15 @@
+count = 0
 primos = []
 numb = int(input('Digite um numero: '))
 
-for num in range(2, numb):  # Começa de 2, pois 1 não é primo
-    if num > 1:
-        for i in range(2, num):
-            if num % i == 0:
-                break  # Se encontrou um divisor, não é primo
-        else:
-            primos.append(num)  # Se o loop terminou sem break, o número é primo
+for num in range(3, numb):
+    notprimo = False
+    for i in range(2, num):
+        if num % i == 0:
+            count += 1
+            notprimo = True
+            break
+    if notprimo == False:
+        primos.append(num)
 
-print(f'Os números primos até {numb} são: {primos}')
+print(f'Os números primos até {numb} são: {primos}, numero de divisoes: {count}')
